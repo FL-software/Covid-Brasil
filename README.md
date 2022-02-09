bbddbfd6c165f09a22289b3373830e0fe555a8b5
brunno51 <bruno51_@hotmail.com>
2022-02-09 14:51:31 -0300

    Projeto covide brasil em andamento

---
+++ README.md
@@ -0,0 +1,43 @@
+# Covid Brasil
+
+
+
+Nesse projeto são utilizados os dados da pandemia de COVID-19 no Brasil. O coronavírus foi identificado pela primeira vez por autoridades da cidade de Wuhan, capital da província de Hubei na China, entre pacientes que tinham desenvolvido pneumonia sem causa identificável. O surto inicial deu origem a uma pandemia global que à data de 25 de setembro de 2021 tinha resultado em 231.105.748 casos confirmados e 4.736.892 mortes em todo o mundo.
+
+Pensa-se que o coronavírus tenha origem zoonótica. A primeira transmissão para seres humanos ocorreu em Wuhan, na China, em novembro ou dezembro de 2019. No início de janeiro de 2020, a principal fonte de infecção era já a transmissão entre seres humanos.
+
+Em 26 de fevereiro de 2020 se confirmou o primeiro caso do covid19 no Brasil. Um brasileiro retornando da Itália, onde a pandemia já tinha alcançado patamares bem preocupantes. Quase 2 anos depois, estamos vivendo um momento histórico, e está literalmente nas nossas mãos o que vai acontecer daqui pra frente.
+
+Você aprenderá como criar um dashboard responsivo usando esse dados, permitindo que se faça uma análise de dados completos sobre o número de casos confirmados, de mortos e de vacinados no Brasil. Ao aprender como montar dashboard com javascript utilizando os dados da covid19, você poderá depois alterar esse projeto para criar até mesmo outros tipos de painel administrativo (admin dashboard).
+
+- CSS
+  - Variáveis CSS
+  - Unidades CSS (rem, %, vh, vw, px)
+  - Metologia BEM
+  - Media Queries
+  - Flexbox
+  - CSS Grid
+  - Pseudo-elementos
+  - Pseudo-classes
+
+- Javascript
+  - Async/Await
+  - Fetch API
+  - Biblioteca de gráficos Apexcharts
+  - Acessar propriedades CSS pelo Javascript
+  - Atributos data-
+  - método parseInt()
+  - .querySelector()
+  - .querySelectorAll()
+  - .addEventListener()
+  - .add()
+  - .remove()
+  - .toggle()
+  - .contains()
+  - .includes()
+  - .forEach()
+  - .setAttribute()
+  - .getAttribute()
+
+E muito mais.
+
---
+++ js/scripts.js
@@ -0,0 +1,105 @@
+const _elements = {
+    loading: document.querySelector(".loading"),
+    switch: document.querySelector(".switch__track"),
+    stateSelectToggle: document.querySelector(".state-select-toggle"),
+    selectOptions: document.querySelectorAll(".state-select-list__item"),
+    selectList: document.querySelector(".state-select-list"),
+    selectToggleIcon: document.querySelector(".state-select-toggle__icon"),
+    selectSearchBox: document.querySelector(".state-select-list__search"),
+    selectStateSelected: document.querySelector(".state-select-toggle__state-selected"),
+    confirmed: document.querySelector(".info__total--confirmed"),
+    deaths: document.querySelector(".info__total--deaths"),
+    deathsDescription: document.querySelector(".data-box__description"),
+    vaccinated1: document.querySelector(".info__total--vaccinated-1"),
+    vaccinated2: document.querySelector(".info__total--vaccinated-2"),
+}
+
+const _data = {
+    id: "brasil=true",
+    vaccinatedInfo: undefined,
+    vaccinated: undefined,
+    confirmed: undefined,
+    deaths: undefined,
+}
+
+const _charts = {};
+
+_elements.switch.addEventListener("click", () => {
+   const isDark = _elements.switch.classList.toggle("switch__track--dark");
+
+   if(isDark) 
+   document.documentElement.setAttribute("data-theme" , "dark");
+   else
+   document.documentElement.setAttribute("data-theme" , "light");
+});
+
+_elements.stateSelectToggle.addEventListener("click", () => {
+    _elements.selectToggleIcon.classList.toggle("state-select-toggle__icon--rotate");
+    _elements.selectList.classList.toggle("state-select-list--show");
+});
+
+_elements.selectOptions.forEach(item => {
+    item.addEventListener("click", () => {
+        _elements.selectStateSelected.innerText = item.innerText;
+        _data.id = item.getAttribute("data-id");
+        _elements.stateSelectToggle.dispatchEvent(new Event("click"));
+    });
+});
+
+_elements.selectSearchBox.addEventListener("keyup", (e) => {
+    const search = e.target.value.toLowerCase();
+
+    for(const item of _elements.selectOptions){
+        const state = item.innerText.toLowerCase();
+
+        if(state.includes(search)){
+            item.classList.remove("state-select-list__item--hide");
+        }
+        else {
+            item.classList.add("state-select-list__item--hide");
+        }
+    }
+});
+
+const request = (api, id) => {
+
+}
+
+const loadData = (id) => {
+
+}
+
+const createBasicChart = (element, config) => {
+
+}
+
+const createDonutChart = (element) => {
+
+}
+
+const createStackedColumnsChart = (element) => {
+
+}
+
+const createCharts = () => {
+
+}
+
+const updateCards = () => {
+
+}
+
+const updateCharts = () => {
+
+}
+
+const getChartOptions = (series, labels, colors) => {
+
+}
+
+const getDonutChartOptions = (value, name, colors) => {
+
+}
+
+loadData(_data.id);
+createCharts();
